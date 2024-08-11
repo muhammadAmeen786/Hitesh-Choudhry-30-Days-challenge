4946ef328013ab6cbdc12215cbc8f5e64945fb90
muhammadAmeen786 <mohdameeendar@gmail.com>
2024-08-11 07:04:30 +0530

    Day 1

---
+++ index.js
@@ -0,0 +1,60 @@
+/*I will try to solve Hitesh Sir's 30 days challenge in order to improve my 
+Programming skills
+*/
+
+//Task 1 : declare a variable  using VAR keyword and assign it a number and log it.
+
+ var varible1 = 786; 
+ console.log(varible1);
+
+
+ //Task 2 : Declare a variable  using LET keyword and assign it a String and log it.
+
+ let variable2 = "Hello JAVASCRIPT";
+ console.log(variable2);
+
+  //Task 3 : Declare a variable  using LET keyword and assign it a Boolean and log it.
+
+const variable3 = true;
+
+console.log(variable3);
+
+//Task4 : declare diff type of variables (boolean , string , array, object etc) and log the typeof them.
+
+let number = 2;
+let string = "Hello we are here";
+let bool = true;
+let array = [2,35,4,3,'string','great']; 
+let obj = {1:"Hello",
+2:"great",
+3:"developers"};
+
+
+console.log(typeof number); //number
+console.log(typeof bool); //boolean
+console.log(typeof string); // string
+console.log(typeof array); // object
+console.log(typeof obj); //object
+
+//Task 5 : Assign a variable a number, then reassing it and log them on console.
+
+let oddNum = 5;
+console.log(oddNum) //5
+oddNum = 3;
+console.log(oddNum); //3
+
+//Task 6: Reassign const variable and observe the error.
+
+const evenNumber = 4;
+evenNumber = 6;//TypeError: Assignment to constant variable.
+console.log(evenNumber) 
+
+
+
+
+
+
+
+
+
+
